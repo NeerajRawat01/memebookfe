635b95625b42047b2d7756ee536069a7d8fe582d
neeraj singh <rawatneeraj925@gmail.com>
2023-05-10 18:34:29 +0530

    fix meme card issue

--- src/components/MemeCard.tsx
+++ src/components/MemeCard.tsx
@@ -8,7 +8,7 @@ const MemeCard: React.FC<props> = ({ name, url, onClick }) => {
   return (
     <div className="bg-blue-300 rounded-sm">
       <div className="flex justify-between text-lg p-2 font-semibold font-mono">
-        <span>{name}</span>
+        <span className=" w-[22rem] truncate">{name}</span>
         <button
           onClick={() => onClick(url, name)}
           className="text-base px-3 py-1 text-white font-normal font-sans bg-green-900 shadow-md rounded-2xl"
